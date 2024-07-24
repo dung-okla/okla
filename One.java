a57ef62c1708076552ab52d96ab7464be6466b92
DoVanDung <dungtob2001@gmail.com>
2024-07-24 23:19:33 +0700

    one

---
+++ One.java
@@ -0,0 +1,14 @@
+package learnjava;
+
+public class One {
+
+	public static void main(String[] args) {
+
+		System.out.println("kkk kjhj jkj");
+		System.out.println("kkk kjhj jkj");
+		System.out.println("kkk kjhj jkj");
+		System.out.println("kkk kjhj jkj");
+		System.out.println("kkk kjhj jkj");
+	}
+
+}
